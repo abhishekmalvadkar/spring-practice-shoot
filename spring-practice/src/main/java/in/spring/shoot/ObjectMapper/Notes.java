61483bcfd7df3ea51bf5460496f6baaa8644030e
abhishek malvadkar ranoji <abhishek@dev.com>
2023-03-07 17:57:46 +0530

    ObjectMapper example deep dive

---
+++ spring-practice/src/main/java/in/spring/shoot/ObjectMapper/Notes.java
@@ -0,0 +1,14 @@
+package in.spring.shoot.ObjectMapper;
+
+public class Notes {
+	
+	/*
+	 * We can use object mapper to convert java object to json string and send to different destinations like
+	 * - To Redis as json string
+	 * - To Kafka as json string
+	 * We can use object mapper to convert json string to java object
+	 * - When data recived from redis into our app then convert json string to java object
+	 * - When data recived from kafka/any message queue into our app then convert json string to java object
+	 */
+
+}
